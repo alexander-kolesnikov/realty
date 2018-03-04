5d6f9da58805c0d460a4899e5a7df4acb4ae5bf6
Kolesnikov Alexander <dave_@mail.ru>
2018-03-04 23:00:05 +0300

    Added urls.py to aggr app, dummy pages index and search

--- aggr/views.py
+++ aggr/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello everyone. Here will be index page")
+
+def search(request):
+    return HttpResponse("That's the search page")
